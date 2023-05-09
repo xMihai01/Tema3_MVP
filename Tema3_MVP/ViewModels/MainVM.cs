a431407cff2f0331001869a1eaab98dc89884ba5
xMihai01 <doloiu22@icloud.com>
2023-05-09 21:48:27 +0300

    Add project files.

---
+++ Tema3_MVP/ViewModels/MainVM.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_MVP.ViewModels
+{
+    public class MainVM
+    {
+        public List<string> personType { get; set; } = new List<string> { "Elev", "Profesor", "Diriginte", "Administrator" };
+
+        public string _userName;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+        public string _password;
+
+        public string PasswordText
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(PasswordText));
+            }
+        }
+
+        public string _personType;
+
+        public string PersonType
+        {
+            get { return _personType; }
+            set
+            {
+                _personType = value;
+                OnPropertyChanged(nameof(PersonType));
+            }
+        }
+        public MainVM()
+        {
+            PersonType = "Elev";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Tema3_MVP/Views/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Tema3_MVP.ViewModels;
+namespace Tema3_MVP.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainVM mainVM;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainVM = new MainVM();
+            DataContext = mainVM;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("OK " + mainVM.UserName + " " + mainVM.PersonType + " " + mainVM.PasswordText);
+        }
+    }
+}
